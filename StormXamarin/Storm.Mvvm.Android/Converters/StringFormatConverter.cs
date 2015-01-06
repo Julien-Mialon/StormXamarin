b49ac8bac6a0266edbcb0e1f733ecf4d20c30b06
Julien Mialon <mialon.julien@gmail.com>
2015-01-06 15:57:41 +0100

    Add last version of packages + add default StringFormatConverter.

---
+++ StormXamarin/Storm.Mvvm.Android/Converters/StringFormatConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Storm.Mvvm.Converters
+{
+	public class StringFormatConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			string format = (parameter as string) ?? string.Empty;
+			return string.Format(format, value);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
