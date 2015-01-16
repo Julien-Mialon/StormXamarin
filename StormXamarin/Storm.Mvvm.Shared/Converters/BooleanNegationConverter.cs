309dcfcc2a0d2293d0c4a19e01c43534dd44cc61
Julien Mialon <mialon.julien@gmail.com>
2015-01-16 16:56:45 +0100

    Add shared project to handle slightly different code for all platforms. (like converters)

---
+++ StormXamarin/Storm.Mvvm.Shared/Converters/BooleanNegationConverter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+#if WINDOWS_APP
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+#else
+using System.Windows;
+using System.Windows.Data;
+using System.Globalization;
+#endif
+
+namespace Storm.Mvvm.Converters
+{
+    public class BooleanNegationConverter : IValueConverter
+    {
+#if WINDOWS_APP
+		public object Convert(object value, Type targetType, object parameter, string language)
+#else
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+#endif
+	    {
+		    bool invert = true;
+
+		    if (parameter != null)
+		    {
+			    if (parameter is string)
+			    {
+				    invert = bool.Parse(parameter as string);
+			    }
+				else if (parameter is bool)
+				{
+					invert = (bool)parameter;
+				}
+		    }
+
+		    bool val = (bool) value;
+		    return invert ? val : !val;
+	    }
+
+#if WINDOWS_APP
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+#else
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+#endif
+	    {
+			bool invert = true;
+
+			if (parameter != null)
+			{
+				if (parameter is string)
+				{
+					invert = bool.Parse(parameter as string);
+				}
+				else if (parameter is bool)
+				{
+					invert = (bool)parameter;
+				}
+			}
+
+			bool val = (bool)value;
+			return invert ? val : !val;
+	    }
+    }
+}
--- StormXamarin/Storm.Mvvm.Shared/Converters/BooleanToVisibilityConverter.cs
+++ StormXamarin/Storm.Mvvm.Shared/Converters/BooleanToVisibilityConverter.cs
@@ -1,13 +1,24 @@
-﻿using System;
-using System.Globalization;
+﻿#if WINDOWS_APP || WINDOWS_PHONE
+using System;
+
+#if WINDOWS_PHONE
 using System.Windows;
 using System.Windows.Data;
+using System.Globalization;
+#elif WINDOWS_APP
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+#endif
 
 namespace Storm.Mvvm.Converters
 {
 	public sealed class BooleanToVisibilityConverter : IValueConverter
 	{
+#if WINDOWS_APP
+		public object Convert(object value, Type targetType, object parameter, string language)
+#else
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+#endif
 		{
 			bool invert = (parameter is bool) && (bool)parameter;
 			bool val = (bool)value;
@@ -15,7 +26,11 @@ namespace Storm.Mvvm.Converters
 			return (val ^ invert) ? Visibility.Visible : Visibility.Collapsed;
 		}
 
+#if WINDOWS_APP
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+#else
 		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+#endif
 		{
 			bool invert = (parameter is bool) && (bool)parameter;
 			Visibility val = (Visibility)value;
@@ -25,3 +40,5 @@ namespace Storm.Mvvm.Converters
 		}
 	}
 }
+
+#endif
