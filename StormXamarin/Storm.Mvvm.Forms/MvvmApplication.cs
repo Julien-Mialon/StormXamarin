774b9544cee206c90460282623352a8be602f5d5
Julien Mialon <mialon.julien@gmail.com>
2015-02-02 18:24:55 +0100

    Fix some issue with Application wide resources in Mvvm.Forms

--- StormXamarin/Storm.Mvvm.Forms/MvvmApplication.cs
+++ StormXamarin/Storm.Mvvm.Forms/MvvmApplication.cs
@@ -6,7 +6,12 @@ namespace Storm.Mvvm
 {
 	public class MvvmApplication : Application
 	{
-		public MvvmApplication(Func<Page> mainPageMaker, Action serviceRegisterCallback = null)
+		/// <summary>
+		/// Initialize MvvmApplication with default service but do not initialize MainPage.
+		/// You need to call InitializeMainPage next !
+		/// </summary>
+		/// <param name="serviceRegisterCallback">Callback to register additional services if needed.</param>
+		public MvvmApplication(Action serviceRegisterCallback = null)
 		{
 			DependencyService.Register<INavigationService, NavigationService>();
 			DependencyService.Register<IDialogService, DialogService>();
@@ -16,11 +21,26 @@ namespace Storm.Mvvm
 			{
 				serviceRegisterCallback();
 			}
+		}
 
-			Page mainPage = mainPageMaker();
+		/// <summary>
+		/// Initialize MvvmApplication with default services and initialize MainPage if mainPageMaker is not null.
+		/// </summary>
+		/// <param name="mainPageMaker">Callback to create the MainPage once all services has been registered</param>
+		/// <param name="serviceRegisterCallback">Callback to register additional services if needed.</param>
+		public MvvmApplication(Func<Page> mainPageMaker, Action serviceRegisterCallback = null) : this(serviceRegisterCallback)
+		{
+			InitializeMainPage(mainPageMaker());
+		}
 
+		protected void InitializeMainPage<TMainPage>() where TMainPage : Page, new()
+		{
+			InitializeMainPage(new TMainPage());
+		}
+
+		protected void InitializeMainPage(Page mainPage)
+		{
 			DependencyService.Get<ICurrentPageService>().Push(mainPage);
-
 			MainPage = new MvvmNavigationPage(mainPage);
 		}
 	}
