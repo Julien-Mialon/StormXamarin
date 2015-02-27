3eaa66ba470af8fe7f05b1b3cb7a5506cb2d7d96
Julien Mialon <mialon.julien@gmail.com>
2015-02-27 14:26:18 +0100

    Add helper class to generate all names

---
+++ StormXamarin/Storm.Binding.AndroidTarget/Helper/NameGeneratorHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace Storm.Binding.AndroidTarget.Helper
+{
+	public static class NameGeneratorHelper
+	{
+		private const string VIEWHOLDER_FORMAT = "AutoGen_ViewHolder_{0}";
+		private static int _viewHolderCounter;
+
+		public static string GetViewHolderName()
+		{
+			return string.Format(VIEWHOLDER_FORMAT, _viewHolderCounter++);
+		}
+	}
+}
