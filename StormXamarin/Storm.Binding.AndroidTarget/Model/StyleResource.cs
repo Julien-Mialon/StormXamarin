9e9f681e4b94fda81ae4f6d1c9c9241107cd6120
Julien Mialon <mialon.julien@gmail.com>
2015-03-10 13:18:46 +0100

    Implement support of styles to simplify view writing.

---
+++ StormXamarin/Storm.Binding.AndroidTarget/Model/StyleResource.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Storm.Binding.AndroidTarget.Model
+{
+	public class StyleResource : Resource
+	{
+		public StyleResource()
+		{
+			
+		}
+
+		public StyleResource(Resource resource) : base(resource.Key)
+		{
+			ResourceElement = resource.ResourceElement;
+			foreach (KeyValuePair<string, string> property in resource.Properties)
+			{
+				Properties.Add(property.Key, property.Value);
+			}
+		}
+	}
+}
