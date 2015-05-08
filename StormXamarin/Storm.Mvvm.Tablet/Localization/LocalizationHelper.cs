e3a9bfcdca4f43d1d84b84f30f0e3cbddc8fe591
Julien Mialon <mialon.julien@gmail.com>
2015-05-08 17:02:20 +0200

    Start implementation for windows 8

---
+++ StormXamarin/Storm.Mvvm.Tablet/Localization/LocalizationHelper.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Windows.UI.Xaml;
+using Storm.Mvvm.Services;
+
+namespace Storm.Mvvm.Localization
+{
+	public static class LocalizationHelper
+	{
+		private const string UidPropertyName = "Uid";
+		private const string PropertiesPropertyName = "Properties";
+
+		public static ILocalizationService ResourceService;
+
+		public static string GetUid(DependencyObject obj)
+		{
+			return (string)obj.GetValue(UidProperty);
+		}
+
+		public static void SetUid(DependencyObject obj, string value)
+		{
+			obj.SetValue(UidProperty, value);
+		}
+
+		public static string GetProperties(DependencyObject obj)
+		{
+			return (string)obj.GetValue(PropertiesProperty);
+		}
+
+		public static void SetProperties(DependencyObject obj, string value)
+		{
+			obj.SetValue(PropertiesProperty, value);
+		}
+
+		public static readonly DependencyProperty UidProperty = DependencyProperty.RegisterAttached(UidPropertyName, typeof(string), typeof(LocalizationHelper), new PropertyMetadata(null, UidChangedCallback));
+		public static readonly DependencyProperty PropertiesProperty = DependencyProperty.RegisterAttached(PropertiesPropertyName, typeof(string), typeof(LocalizationHelper), new PropertyMetadata(null, PropertiesChangedCallback));
+
+		private static void UidChangedCallback(object sender, DependencyPropertyChangedEventArgs args)
+		{
+#if DEBUG
+			if (DesignerProperties.IsInDesignTool)
+			{
+				return;
+			}
+#endif
+			var attachedObject = sender as DependencyObject;
+			var uid = args.NewValue as string;
+
+			if (attachedObject != null)
+			{
+				string properties = GetProperties(attachedObject);
+
+				if (!String.IsNullOrEmpty(properties))
+				{
+					UpdateProperties(attachedObject, properties, uid);
+				}
+			}
+		}
+
+		private static void PropertiesChangedCallback(object sender, DependencyPropertyChangedEventArgs args)
+		{
+#if DEBUG
+			if (DesignerProperties.IsInDesignTool)
+			{
+				return;
+			}
+#endif
+			var attachedObject = sender as DependencyObject;
+			var properties = args.NewValue as string;
+
+			if (attachedObject != null)
+			{
+				string uid = GetUid(attachedObject);
+
+				UpdateProperties(attachedObject, properties, uid);
+			}
+		}
+
+		private static void UpdateProperties(DependencyObject attachedObject, string properties, string uid)
+		{
+#if DEBUG
+			if (DesignerProperties.IsInDesignTool)
+			{
+				return;
+			}
+#endif
+			IEnumerable<string> props = properties.Split(',');
+
+			Type attachedType = attachedObject.GetType();
+			bool hasProperties = props.Any();
+
+			foreach (PropertyInfo propInfo in props.Select(attachedType.GetRuntimeProperty).Where(propInfo => propInfo != null))
+			{
+				string value = hasProperties ? ResourceService.GetString(uid, properties) : ResourceService.GetString(uid);
+				propInfo.SetValue(attachedObject, value);
+			}
+		}
+	}
+}
---
+++ StormXamarin/Storm.Mvvm.Tablet/Services/LocalizationService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Resources;
+
+namespace Storm.Mvvm.Services
+{
+	public class LocalizationService : ILocalizationService
+	{
+		private readonly ResourceLoader _resourceLoader;
+
+
+		public LocalizationService()
+		{
+			_resourceLoader = new ResourceLoader();
+		}
+
+		public string GetString(string uid)
+		{
+			if (string.IsNullOrEmpty(uid))
+			{
+				return "";
+			}
+			return _resourceLoader.GetString(uid);
+		}
+
+		public string GetString(string uid, string property)
+		{
+			if (string.IsNullOrEmpty(uid))
+			{
+				return "";
+			}
+			string key = string.Format("{0}_{1}", uid, property);
+			return _resourceLoader.GetString(key);
+		}
+	}
+}
