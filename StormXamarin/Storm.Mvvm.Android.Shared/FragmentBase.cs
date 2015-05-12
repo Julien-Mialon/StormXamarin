a4c7b4a188601dafc093c79f06f6e4949043ae8d
Julien Mialon <mialon.julien@gmail.com>
2015-05-12 15:31:51 +0200

    Fix navigation issue with Fragment and Activity in Android when the ViewModel is set after the OnStart call.

--- StormXamarin/Storm.Mvvm.Android.Shared/FragmentBase.cs
+++ StormXamarin/Storm.Mvvm.Android.Shared/FragmentBase.cs
@@ -1,23 +1,44 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-
+using Android.OS;
+using Android.Views;
+using Storm.Mvvm.Bindings;
+using Storm.Mvvm.Services;
 #if SUPPORT
 using Android.Support.V4.App;
 #else
 using Android.App;
 #endif
-using Android.OS;
-using Android.Views;
-using Storm.Mvvm.Bindings;
 
 namespace Storm.Mvvm
 {
 	public abstract class FragmentBase : Fragment, INotifyPropertyChanged
 	{
-		protected ViewModelBase ViewModel { get; private set; }
+		private ViewModelBase _viewModel;
+		protected ViewModelBase ViewModel
+		{
+			get { return _viewModel; }
+			private set
+			{
+				if (!Equals(_viewModel, value))
+				{
+					_viewModel = value;
+
+					if (_activityState == ActivityState.Running && value != null)
+					{
+						value.OnNavigatedTo(new NavigationArgs(NavigationArgs.NavigationMode.New), ParametersKey);
+					}
+				}
+			}
+		}
 
 		protected View RootView { get; private set; }
+
+		public string ParametersKey { get; set; }
+
+		private ActivityState _activityState = ActivityState.Uninitialized;
+
 
 		public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 		{
@@ -50,6 +71,51 @@ namespace Storm.Mvvm
 		{
 			return new List<BindingObject>();
 		}
+
+		#region Lifecycle implementation
+
+		public override void OnStart()
+		{
+			base.OnStart();
+			SetViewModel(CreateViewModel());
+			if (ViewModel != null && _activityState != ActivityState.Running)
+			{
+				ViewModel.OnNavigatedTo(new NavigationArgs(NavigationArgs.NavigationMode.New), ParametersKey);
+			}
+			_activityState = ActivityState.Running;
+		}
+
+		public override void OnResume()
+		{
+			base.OnResume();
+			if (ViewModel != null && _activityState != ActivityState.Running)
+			{
+				ViewModel.OnNavigatedTo(new NavigationArgs(NavigationArgs.NavigationMode.Back), ParametersKey);
+			}
+			_activityState = ActivityState.Running;
+		}
+
+		public override void OnPause()
+		{
+			base.OnPause();
+			if (ViewModel != null && _activityState != ActivityState.Stopped)
+			{
+				ViewModel.OnNavigatedFrom(new NavigationArgs(NavigationArgs.NavigationMode.Forward));
+			}
+			_activityState = ActivityState.Stopped;
+		}
+
+		public override void OnStop()
+		{
+			base.OnStop();
+			if (ViewModel != null && _activityState != ActivityState.Stopped)
+			{
+				ViewModel.OnNavigatedFrom(new NavigationArgs(NavigationArgs.NavigationMode.Back));
+			}
+			_activityState = ActivityState.Stopped;
+		}
+
+		#endregion
 
 		#region INotifyPropertyChanged implementation
 
