cbcdf8024dd7d38d10124d9e140d131497001280
Julien Mialon <julien.mialon@backelite.com>
2015-09-04 12:30:23 +0200

    Project setup

--- StormXamarin/Storm.MvvmCross.Android.Support.v7/Properties/AssemblyInfo.cs
+++ StormXamarin/Storm.MvvmCross.Android.Support.v7/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Storm.Mvvm.Android.Support")]
+[assembly: AssemblyTitle("Storm.MvvmCross.Android.Support.v7")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Storm.Mvvm.Android.Support")]
+[assembly: AssemblyProduct("Storm.MvvmCross.Android.Support.v7")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
